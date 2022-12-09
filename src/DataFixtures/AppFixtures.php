d8222573c24eb4b6e305c179b9658a847e8fc0d3
VAR100VIN <98034656+VAR100VIN@users.noreply.github.com>
2022-12-09 15:42:16 +0100

    test

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,14 +4,43 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Plant;
+use App\Entity\User;
+use App\Entity\PlantImages;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
-    {
+    public function load(ObjectManager $manager){   
         // $product = new Product();
         // $manager->persist($product);
+        
 
-        $manager->flush();
+            $data = './src/DataFixtures/Ressources/Models/plants.json';
+            $json = file_get_contents($data);
+            print_r($json);
+            $json2 = json_decode($json,true);
+
+            print_r($json2);
+   
+            
+            
+            foreach($tableauplant as $plantName) {
+                $plant = new Plant();
+                $plant->setName($plantName);
+                $plant->setLevel();//a changer
+                $plant->setDisplay(1);
+                $manager->persist($plant);
+                $manager->flush();
+                $j=0;
+                foreach($tableauImages[$plantName] as $image){
+                $j+=1;
+                $plantImage=new PlantImages();
+                $plantImage->setPlant($plant);
+                $plantImage->setImage("fixtures/".$image);
+                $manager->persist($plantImage);
+                $manager->flush();
+            };      
+      }
     }
 }
+
